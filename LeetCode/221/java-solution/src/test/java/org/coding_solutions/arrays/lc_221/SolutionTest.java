4e5e69e17ef107d1d78ba34e19521a0953cab9fb
Suhas Krishna Srivastava <suhasCanDoSo@gmail.com>
2024-02-17 00:26:51 +0530

    array-lc-221-java: Better use of TestPrep.

--- LeetCode/221/java-solution/src/test/java/org/coding_solutions/arrays/lc_221/SolutionTest.java
+++ LeetCode/221/java-solution/src/test/java/org/coding_solutions/arrays/lc_221/SolutionTest.java
@@ -1,26 +1,32 @@
 package org.coding_solutions.arrays.lc_221;
 
 import org.coding_solutions.arrays.lc_221.model.JSONTestData;
-import sks.utilities.test_prep.TestPrep;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
+import sks.utilities.test_prep.TestPrep;
 
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.List;
 
 public class SolutionTest {
     private final static Path testFileDir = Path.of("..", "test-data");
     private final Solution sut = new Solution();
 
-    @ParameterizedTest
-    @MethodSource("jsonTestData")
-    public void testAgainstFiles(JSONTestData jsonTestData) {
-        int maxSquare = sut.maxSquare(jsonTestData.inputData());
-        assert jsonTestData.output() == maxSquare*maxSquare;
+    /**
+     * Always statically initialise {@link TestPrep#testDirectory} or any other {@link TestPrep} resource because its
+     * methods need to be called statically in {@link MethodSource}. The same can be done in a static block.
+     */
+    @BeforeAll
+    public static void init() {
+        TestPrep.testDirectory = testFileDir;
     }
 
-    public static List<JSONTestData> jsonTestData() throws IOException {
-        return TestPrep.jsonTestData(testFileDir, JSONTestData.class);
+    @ParameterizedTest
+    @MethodSource("sks.utilities.test_prep.TestPrep#jsonFilesInDir")
+    public void testJSONAgainstFiles(String jsonFilePath) throws IOException {
+        JSONTestData obj = TestPrep.jsonFileToData(jsonFilePath, JSONTestData.class);
+        int maxSquare = sut.maxSquare(obj.inputData());
+        assert obj.output() == maxSquare*maxSquare;
     }
 }
