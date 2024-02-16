8ea21c7eaa0bb0d0cae5fcd4d32bf4900e15023c
Suhas Krishna Srivastava <suhasCanDoSo@gmail.com>
2024-02-17 00:23:41 +0530

    array-lc-221-java: No need for null record.

--- LeetCode/221/java-solution/src/main/java/org/coding_solutions/arrays/lc_221/model/JSONTestData.java
+++ LeetCode/221/java-solution/src/main/java/org/coding_solutions/arrays/lc_221/model/JSONTestData.java
@@ -2,8 +2,4 @@ package org.coding_solutions.arrays.lc_221.model;
 
 record CharSquareCount(char c, int count, int sqCount) {}
 
-public record JSONTestData(char[][] inputData, CharSquareCount[][] charCountData, int output) {
-    public JSONTestData(char[][] inputData, int output) {
-        this(inputData, null, output);
-    }
-}
+public record JSONTestData(char[][] inputData, CharSquareCount[][] charCountData, int output) {}
